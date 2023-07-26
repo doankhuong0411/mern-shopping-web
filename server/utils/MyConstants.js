07c1316aa4dd70a8c38b94173f0620af069043f2
doankhuong0411 <140500225+doankhuong0411@users.noreply.github.com>
2023-07-26 10:29:04 +0700

    Update MyConstants.js

--- server/utils/MyConstants.js
+++ server/utils/MyConstants.js
@@ -1,12 +1,11 @@
 const MyConstants = {
-  DB_SERVER: 'cluster.n7ltsaw.mongodb.net',
-  DB_USER: '207CT28034',
-  DB_PASS: '207CT28034',
-  DB_DATABASE: 'admins',
+  DB_SERVER: 'shopping-online.wglxpmf.mongodb.net',
+  DB_USER: '207ct10204',
+  DB_PASS: '59v314844',
+  DB_DATABASE: 'shoppingonline',
   EMAIL_USER: '<email_user>', // Microsoft mail service
   EMAIL_PASS: '<email_pass>',
   JWT_SECRET: '121120',
   JWT_EXPIRES: '1800000', // in milliseconds
 };
 module.exports = MyConstants;
-
